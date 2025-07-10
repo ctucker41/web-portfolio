7745260d135053e614ee921bb6fc255effd7291a
Christian Tucker <149714466+ctucker41@users.noreply.github.com>
2025-07-10 15:51:18 -0400

    Updated Planned Projects card with more current info

--- src/components/ui/sectionOne.tsx
+++ src/components/ui/sectionOne.tsx
@@ -14,42 +14,46 @@ export default function SectionOne() {
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 1, ease: "easeOut", delay: 5.5 }}
             >
-            <Card className="p-8 w-[90%] mx-auto md:h-[1000px] md:w-[550px] bg-gray-900 border-slate-600 border-[1px] flex flex-col items-center">
+                <Card className="p-8 w-[90%] mx-auto md:h-[1000px] md:w-[550px] bg-gray-900 border-slate-600 border-[1px] flex flex-col items-center">
 
-                <h2 className="text-xl  md:text-3xl flex justify-center items-center text-purple-300 font-bold">Planned Projects</h2>
-                <p className="mt-4 md:mt-16 flex flex-row text-white text-xs md:text-lg leading-relaxed indent-6">
-                    <img src="/python.svg" alt="Python Logo" className="w-6 h-6 mr-0 align-middle" />
-                    A Python script that scrapes job postings from websites based on keywords, extracts relevant details, and saves them for easy review.
-                </p>
+                    <h2 className="text-xl  md:text-3xl flex justify-center items-center text-purple-300 font-bold">Planned Projects</h2>
 
-                <p className="mt-4 md:mt-16 flex flex-row text-white text-xs md:text-lg leading-relaxed indent-6">
-                    <img src="/ruby.svg" alt="Ruby Logo" className="w-5 h-5 align-middle" />
-                    A lightweight and customizable Ruby on Rails CRUD application to manage tasks and lists.
-                </p>
+                    <span className="text-fuchsia-500 font-semibold mt-4 md:mt-16">In Progress</span>
+                    <p className="flex flex-row text-white text-xs md:text-lg leading-relaxed indent-6">
+                        <img src="/powershell.svg" alt="PowerShell Logo" className="w-5 h-5 align-middle" />
+                        A PowerShell script that collects system information and outputs it as a report. Optionally delivers warnings based on certain
+                        conditions such as high temperatures.
+                    </p>
 
-                <p className="mt-4 md:mt-16 flex flex-row text-white text-xs md:text-lg leading-relaxed indent-6">
-                    <img src="/bash.svg" alt="Bash Logo" className="w-6 h-6 align-middle" />
-                    A Bash script that installs essential development tools and dependencies on a fresh Linux installation. Currently deciding between
-                    using Ubuntu or Arch Linux as the base system.
-                </p>
+                    <div className="w-full border-t-[1px] border-fuchsia-500 mt-4" />
 
-                <p className="mt-4 md:mt-16 flex flex-row text-white text-xs md:text-lg leading-relaxed indent-6">
-                    <img src="/powershell.svg" alt="PowerShell Logo" className="w-5 h-5 align-middle" />
-                    A PowerShell script that collects system information and outputs it as a report. Optionally delivers warnings based on certain
-                    conditions such as high temperatures.
-                </p>
+                    <p className="mt-4 md:mt-8 flex flex-row text-white text-xs md:text-lg leading-relaxed indent-6">
+                        <img src="/python.svg" alt="Python Logo" className="w-6 h-6 mr-0 align-middle" />
+                        A Python script that scrapes job postings from websites based on keywords, extracts relevant details, and saves them for easy review.
+                    </p>
 
-                <p className="mt-4 md:mt-16 flex flex-row text-white text-xs md:text-lg leading-relaxed indent-6">
-                    <img src="/cpp.svg" alt="C++ Logo" className="w-6 h-6 align-middle" />
-                    C++ code for a simple text-based adventure game with a focus on object-oriented design.
-                </p>
+                    <p className="mt-4 md:mt-16 flex flex-row text-white text-xs md:text-lg leading-relaxed indent-6">
+                        <img src="/ruby.svg" alt="Ruby Logo" className="w-5 h-5 align-middle" />
+                        A lightweight and customizable Ruby on Rails CRUD application to manage tasks and lists.
+                    </p>
 
-                <p className="mt-4 md:mt-16 flex flex-row text-white text-xs md:text-lg leading-relaxed indent-6">
-                    <img src="/c.svg" alt="C Logo" className="w-6 h-6 align-middle" />
-                    A C-based command-line tool that analyzes text files—counting words, characters, lines, and more—and outputs a summary report.
-                </p>
+                    <p className="mt-4 md:mt-16 flex flex-row text-white text-xs md:text-lg leading-relaxed indent-6">
+                        <img src="/bash.svg" alt="Bash Logo" className="w-6 h-6 align-middle" />
+                        A Bash script that installs essential development tools and dependencies on a fresh Linux installation. Currently deciding between
+                        using Ubuntu or Arch Linux as the base system.
+                    </p>
 
-            </Card>
+                    <p className="mt-4 md:mt-16 flex flex-row text-white text-xs md:text-lg leading-relaxed indent-6">
+                        <img src="/c.svg" alt="C Logo" className="w-6 h-6 align-middle" />
+                        A C-based command-line tool that analyzes text files—counting words, characters, lines, and more—and outputs a summary report.
+                    </p>
+
+                    <p className="mt-4 md:mt-16 flex flex-row text-white text-xs md:text-lg leading-relaxed indent-6">
+                        <img src="/cpp.svg" alt="C++ Logo" className="w-6 h-6 align-middle" />
+                        C++ code for a simple text-based adventure game with a focus on object-oriented design.
+                    </p>
+
+                </Card>
             </motion.div>
 
             <div className="flex flex-col justify-between md:h-[1000px]">
